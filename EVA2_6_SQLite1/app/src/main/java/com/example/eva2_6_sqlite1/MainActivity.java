354afed293569a9d2522281f34fd319866fd2843
Jofiel Batista Farias <jofielbatista@hotmail.com>
2020-04-03 13:05:17 -0700

    EXERCISE 6 UPDATED

--- EVA2_6_SQLite1/app/src/main/java/com/example/eva2_6_sqlite1/MainActivity.java
+++ EVA2_6_SQLite1/app/src/main/java/com/example/eva2_6_sqlite1/MainActivity.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -20,13 +21,39 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             db = this.openOrCreateDatabase("db",MODE_PRIVATE,null);
-            // here you do something with your database ...
+            //CREATE TABLED AND ADD A USER
+            createTable("usuario","usuario@usuariomail.com");
+
             db.close();
             txtMsg.append(db.getPath());
             txtMsg.append("\nDB created");
         } catch (SQLiteException e) {
             txtMsg.append("\nERROR " + e.getMessage());
         }
+    }
+
+
+    private void createTable(String uName, String uEmail){
+        String msg = "USER SUCEFULLY CREATED";
+        db.beginTransaction();
+        try {
+            //perform your database operations here ...
+            db.execSQL("CREATE TABLE IF NOT EXISTS PEOPLE ("
+                    + " recID integer PRIMARY KEY autoincrement, "
+                    + " name text, "
+                    + " email text ); " );
+            db.execSQL( "insert into PEOPLE(name, email) values ( '"+ uName +"', '"+uEmail+"');" );
+            db.setTransactionSuccessful(); //commit your changes
+        }
+        catch (SQLiteException e) {
+            //report problem
+            msg = "ERROR: " + e.getMessage();
+        }
+        finally {
+            db.endTransaction();
+            Toast.makeText(this, msg,Toast.LENGTH_LONG).show();
+        }
+
     }
 }
 
